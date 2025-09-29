60069110b9d21af3811a3059b516cdfd9eaa5f2c
Marylou Lohier <mlohier001@iutbayonne.univ-pau.fr>
2025-09-29 10:37:08 +0200

    Mise en place et test de la méthode generate

--- src/main/java/com/lohier/facteursPremiers/FacteursPremiers.java
+++ src/main/java/com/lohier/facteursPremiers/FacteursPremiers.java
@@ -1,5 +1,30 @@
 package com.lohier.facteursPremiers;
 
-public class FacteursPremiers {
+import java.util.HashSet;
+import java.util.Set;
 
+import static java.lang.Math.round;
+import static java.lang.Math.sqrt;
+
+public class FacteursPremiers {
+    public Set<Integer> generate(int pNb)
+    {
+        Set<Integer> liste = new HashSet<>();
+        while (pNb % 2 == 0) {
+            liste.add(2);
+            pNb /= 2;
+        }
+        for (int i = 3; i < round(sqrt(pNb)); i+=2) {
+            while (pNb % i == 0)
+            {
+                liste.add(i);
+                pNb /= i;
+            }
+        }
+        if (pNb > 2) {
+            liste.add(pNb);
+        }
+
+        return liste;
+    }
 }
--- src/test/java/com/lohier/facteursPremiersTest/FacteursPremiersTest.java
+++ src/test/java/com/lohier/facteursPremiersTest/FacteursPremiersTest.java
@@ -1,5 +1,62 @@
 package com.lohier.facteursPremiersTest;
 
+import com.lohier.facteursPremiers.FacteursPremiers;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class FacteursPremiersTest {
 
+    FacteursPremiers facteursPremiers = new FacteursPremiers();
+    @Test
+    void devrait_renvoyer_une_liste_vide()
+    {
+        //GIVEN
+        int nb1 = 0;
+        int nb2 = 1;
+        Set<Integer> resultAttendu = new HashSet<>();
+
+
+        //WHEN
+        Set<Integer> result1 = facteursPremiers.generate(nb1);
+        Set<Integer> result2 = facteursPremiers.generate(nb2);
+
+        //THEN
+        assertThat(result1).isEqualTo(resultAttendu);
+        assertThat(result2).isEqualTo(resultAttendu);
+    }
+
+    @Test
+    void devrait_renvoyer_une_liste_avec_seulement_deux()
+    {
+        //GIVEN
+        int nb = 2;
+        Set<Integer> resultAttendu = new HashSet<>();
+        resultAttendu.add(2);
+
+        //WHEN
+        Set<Integer> result = facteursPremiers.generate(nb);
+
+        //THEN
+        assertThat(result).isEqualTo(resultAttendu);
+    }
+
+    @Test
+    void devrait_renvoyer_une_liste_valide()
+    {
+        //GIVEN
+        int nb = 6;
+        Set<Integer> resultAttendu = new HashSet<>();
+        resultAttendu.add(2);
+        resultAttendu.add(3);
+
+        //WHEN
+        Set<Integer> result = facteursPremiers.generate(nb);
+
+        //THEN
+        assertThat(result).isEqualTo(resultAttendu);
+    }
 }
