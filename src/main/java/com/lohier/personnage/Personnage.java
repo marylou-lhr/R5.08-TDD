a2eef6fd5abc314793dfec9aa7abd17227bddd3c
Marylou Lohier <mlohier001@iutbayonne.univ-pau.fr>
2025-09-29 11:47:00 +0200

    Mise en place de deux tests dont un scénario nominal et un autre

--- src/main/java/com/lohier/personnage/Personnage.java
+++ src/main/java/com/lohier/personnage/Personnage.java
@@ -1,4 +1,19 @@
 package com.lohier.personnage;
 
+import java.util.ArrayList;
+
 public class Personnage {
+
+    public
+        String tourner(int pNbFois)
+        {
+            ArrayList<String> orientations = new ArrayList<>();
+
+            orientations.add("NORD");
+            orientations.add("EST");
+            orientations.add("SUD");
+            orientations.add("OUEST");
+
+            return orientations.get(pNbFois);
+        }
 }
--- src/test/java/com/lohier/personnage/PersonnageTest.java
+++ src/test/java/com/lohier/personnage/PersonnageTest.java
@@ -1,4 +1,39 @@
 package com.lohier.personnage;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class PersonnageTest {
+
+    Personnage personnage = new Personnage();
+
+    @Test
+    void devrait_retourner_EST()
+    {
+        //GIVEN
+        int nbFois = 3;
+        String resultAttendu = "OUEST";
+
+        //WHEN
+        String resultat = personnage.tourner(nbFois);
+
+        //THEN
+        assertThat(resultat).isEqualTo(resultAttendu);
+    }
+
+    @Test
+    void devrait_retourner_NORD()
+    {
+        //GIVEN
+        int nbFois = 4;
+        String resultAttendu = "NORD";
+
+        //WHEN
+        String resultat = personnage.tourner(nbFois);
+
+        //THEN
+        assertThat(resultat).isEqualTo(resultAttendu);
+    }
+
 }
